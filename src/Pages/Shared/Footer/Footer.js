ad87421485fbd425517fe5e13874c9664f3c922f
Miraz9907 <meraz.rahman99@gmail.com>
2022-10-27 16:12:50 +0600

    simple footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -5,23 +5,21 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="bg-dark text-center text-white mt-5">
-  
-  <div className="container p-4">
-   
-   
-   
-    <section className="mb-4">
-      <h2>Tutor Academy</h2>
-      <p>Explore your skills with us and get your dream skill job.</p>
-    </section>
+        <div className="container p-4">
+          <section className="mb-4">
+            <h2>Tutor Academy</h2>
+            <p>Explore your skills with us and get your dream skill job.</p>
+          </section>
+        </div>
 
-  </div>
-  
-  <div className="text-center p-3" style={{backgroundColor:"rgba(0, 0, 0, 0.2"}}>
-    © 2022 Copyright: All right riserved
-    <p className="text-info" >Tutor Academy</p>
-  </div>
-</footer>
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2" }}
+        >
+          © 2022 Copyright: All right riserved
+          <p className="text-info">Tutor Academy</p>
+        </div>
+      </footer>
     );
 };
 
